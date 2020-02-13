ac2beb6980d70961f782088c4936bb922db94a16
afzal-mohammed <49365873+afzal-mohammed@users.noreply.github.com>
2020-02-13 22:50:59 +0530

    Add files via upload

--- css/index.js
+++ css/index.js
@@ -9,13 +9,40 @@ $(document).ready(function(){
   });
 });
 
-$(window).scroll(function() {
-  		if ($(document).scrollTop() > 50) {
-				$('.fixed-top').addClass('affix');
-				console.log("OK");
-			} else {
-				$('.nav').removeClass('affix');
-			}
-		});
 
-   
+// myNav = document.getElementById("navHeader");
+// window.onscroll = (function() {
+//   		if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+// 				document.getElementById("navHeader").addClass('affix');
+// 				console.log("OK");
+// 			} else {
+// 				document.getElementById("navHeader").removeClass('affix');
+// 			}
+// 		});
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+    //Get the button:
+    mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 500px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
